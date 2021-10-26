2d93e035aa6307e4d598819579271105fb06e887
pandaicoding <sumari.w4@gmail.com>
2021-10-26 09:18:21 +0700

    edit route web

--- resources/js/Pages/Dashboard.js
+++ resources/js/Pages/Dashboard.js
@@ -11,7 +11,7 @@ export default function Dashboard(props) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
+                            Dashboard
                         </div>
                     </div>
                 </div>
